2a0cc5835f859ae8241e0928119f1159e03dbb0a
nullrun123 <165893187+nullrun123@users.noreply.github.com>
2024-04-09 20:27:50 +0700

    project start

---
+++ Assets/Scenes/Script/PickUpScript.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpScript : MonoBehaviour
+{
+    public GameObject player;
+    public Transform holdPos;
+   
+    public float throwForce = 500f; 
+    public float pickUpRange = 5f; 
+    private GameObject heldObj; 
+    private Rigidbody heldObjRb;
+    private bool canDrop = true; 
+    private int LayerNumber;
+
+   
+    void Start()
+    {
+        LayerNumber = LayerMask.NameToLayer("holdLayer"); 
+        
+    }
+
+    public void OnPickUpButtonClick()
+    {
+        if (heldObj == null) 
+        {
+            
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, pickUpRange))
+            {
+               
+                if (hit.transform.gameObject.tag == "canPickUp")
+                {
+                    
+                    PickUpObject(hit.transform.gameObject);
+                }
+            }
+        }
+        else
+        {
+            if (canDrop == true)
+            {
+                StopClipping();  
+                DropObject();
+            }
+        }
+
+        if (heldObj != null) 
+        {
+            MoveObject(); 
+        }
+    }
+
+  
+    void PickUpObject(GameObject pickUpObj)
+    {
+        if (pickUpObj.GetComponent<Rigidbody>()) //make sure the object has a RigidBody
+        {
+            heldObj = pickUpObj; //assign heldObj to the object that was hit by the raycast (no longer == null)
+            heldObjRb = pickUpObj.GetComponent<Rigidbody>(); //assign Rigidbody
+            heldObjRb.isKinematic = true;
+            heldObjRb.transform.parent = holdPos.transform; //parent object to holdposition
+            heldObj.layer = LayerNumber; //change the object layer to the holdLayer
+           
+            Physics.IgnoreCollision(heldObj.GetComponent<Collider>(), player.GetComponent<Collider>(), true);
+        }
+    }
+    void DropObject()
+    {
+        //re-enable collision with player
+        Physics.IgnoreCollision(heldObj.GetComponent<Collider>(), player.GetComponent<Collider>(), false);
+        
+        heldObjRb.isKinematic = false;
+        heldObj.transform.parent = null; //unparent object
+        heldObj = null; //undefine game object
+    }
+    void MoveObject()
+    {
+        //keep object position the same as the holdPosition position
+        heldObj.transform.position = holdPos.transform.position;
+    }
+    
+    void ThrowObject()
+    {
+        
+        Physics.IgnoreCollision(heldObj.GetComponent<Collider>(), player.GetComponent<Collider>(), false);
+        
+        heldObjRb.isKinematic = false;
+        heldObj.transform.parent = null;
+        heldObjRb.AddForce(transform.forward * throwForce);
+        heldObj = null;
+    }
+   void StopClipping() //function only called when dropping/throwing
+    {
+        var clipRange = Vector3.Distance(heldObj.transform.position, transform.position);
+        
+        RaycastHit[] hits;
+        hits = Physics.RaycastAll(transform.position, transform.TransformDirection(Vector3.forward), clipRange);
+       
+        if (hits.Length > 1)
+        {
+           
+            heldObj.transform.position = transform.position + new Vector3(0f, -0.5f, 0f); 
+        }
+    }
+    public void OnThrowButtonClick()
+    {
+        if (heldObj != null && canDrop == true)
+        {
+            StopClipping();
+            ThrowObject();
+        }
+    }
+}
+
+   
---
+++ Assets/Scenes/Script/sellobj2.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+using UnityEngine.UI;
+using static UnityEngine.GraphicsBuffer;
+
+
+public class sellitem : MonoBehaviour
+{
+    public Text scoreText;
+    private int score = -10;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // ตรวจสอบว่าวัตถุที่ชนมาอยู่ใน Layer ที่ต้องการหรือไม่
+        if (other.gameObject.layer == LayerMask.NameToLayer("Layer1"))
+        {
+            Debug.Log("ชน");
+            score += 10;
+            scoreText.text = "Score : " + score.ToString();
+            Destroy(other.gameObject);
+        }
+        else if (other.gameObject.layer == LayerMask.NameToLayer("Layer2"))
+        {
+            Debug.Log("ชน");
+            score += 50;
+            scoreText.text = "Score : " + score.ToString();
+            Destroy(other.gameObject);
+        }
+    }
+}
+
+  
